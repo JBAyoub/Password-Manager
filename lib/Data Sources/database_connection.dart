7c64cac46b3656bb4e1881ae2c2d59ee206e84d0
Ayb <aymojobe@gmail.com>
2026-07-07 17:44:22 +0100

    moved the database sensitive data to .env file

--- lib/Data Sources/database_connection.dart
+++ lib/Data Sources/database_connection.dart
@@ -1,23 +1,30 @@
 import 'package:postgres/postgres.dart';
+import 'package:dotenv/dotenv.dart';
 
 class DatabaseConnection {
   Connection? _connection;
+  final _requiredEnvVars = ['host', 'port', 'database', 'username', 'password'];
+  bool get hasEnv => env.isEveryDefined(_requiredEnvVars);
+  var env = DotEnv(includePlatformEnvironment: true)..load();
+
   Future<void> connect() async {
     if (_connection != null) {
       print("Database is already connected.");
       return;
     }
     print("Connecting to the database...");
-    _connection = await Connection.open(
-      Endpoint(
-        host: 'localhost',
-        port: 5432,
-        database: 'password_manager',
-        username: 'postgres',
-        password: 'takhrafih',
-      ),
-      settings: const ConnectionSettings(sslMode: SslMode.disable),
-    );
+    if (hasEnv) {
+      _connection = await Connection.open(
+        Endpoint(
+          host: env['host']!,
+          port: int.parse(env['port']!),
+          database: env['database']!,
+          username: env['username']!,
+          password: env['password']!,
+        ),
+        settings: const ConnectionSettings(sslMode: SslMode.disable),
+      );
+    }
   }
 
   Connection get connection {
